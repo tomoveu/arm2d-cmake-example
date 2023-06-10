0670e3f50da75dee3a0ade825e1829111aa3eda4
Dimi Tomov <dimi@tpm.dev>
2023-06-10 10:03:00 +0300

    Create required headers for arm2d config and compilation
    Signed-off-by: Dimi Tomov <dimi@tpm.dev>

---
+++ RTE_Components.h
@@ -0,0 +1,5 @@
+#define RTE_Acceleration_Arm_2D
+#define __RTE_ACCELERATION_ARM_2D__ /* this macro is deprecated, adding it just in case */
+
+#define RTE_Acceleration_Arm_2D_Helper_Disp_Adapter0
+#define __RTE_ACCELERATION_ARM_2D_HELPER_DISP_ADAPTER0__ /* this macro is deprecated, adding it just in case */
